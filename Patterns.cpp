8e14f9110b320898e88a7b6f2f4065ce8f107602
Deepak Tyagi <160519381+deepaktyagi1@users.noreply.github.com>
2026-03-25 23:03:31 +0530

    Add files via upload

---
+++ Patterns.cpp
@@ -0,0 +1,233 @@
+#include <iostream>
+using namespace std;
+
+
+int main () {
+    int n;
+    cout<<"Enter No. of Rows : ";
+    cin>>n;
+
+
+
+    //   1.           *********************Square patterns***********************
+    
+    
+
+
+   /* for (int i=1;i<=n;i++) {
+        for (int j=1;j<=n;j++) {              // star  square pattern
+            cout<<"* ";
+        }
+        cout<<endl;
+    } */
+
+
+
+    /* for (int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++) {
+            cout<<"1 ";
+        }
+        cout<<endl;
+    }                  */
+
+
+
+   /* for (int i=1;i<=n;i++) {
+        for(int j=1;j<=n;j++) {
+            cout<<"A ";
+        }
+        cout<<endl; 
+    }    */
+
+
+
+   /* for(int i=1;i<=n;i++) {
+        for(int j=1;j<=n;j++) {
+            cout<<j;
+        }
+        cout<<endl;
+    }    */
+    
+
+
+   /*  for (int i=1;i<=n;i++) {
+        char ch ='A';
+        for(int j=1;j<=n;j++) {
+            cout<<ch;
+            ch++;
+        }
+        cout<<endl;
+    }      */
+     
+
+
+    /* int num = 1;
+    for (int i=1;i<=n;i++) {
+        for(int j=1;j<=n;j++) {
+            cout<<num;
+        }
+        cout<<endl;
+        num++;
+    } */
+
+
+
+   /* char ch='A';
+    for(int i=1; i<=n;i++) {
+        for (int j=1;j<=n;j++) {
+            cout<<ch;
+        }
+        cout<<endl;
+        ch++;
+    } */
+
+
+
+     /*  int num=1;
+    for (int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++) {
+            cout<<num;
+            num++;
+        }
+        cout<<endl;
+    }    */
+
+
+
+   /*  char ch='A';
+     for(int i=1;i<=n;i++) {
+        for (int j=1;j<=n;j++) {
+            cout<<ch<<" ";
+            ch++;
+        }
+        cout<<endl;
+     }   */
+
+
+
+
+    //  2.                   ******************Triangle Paterns*********************
+
+
+
+  /*  for( int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++) {
+            cout<<"*";                  // star triangle pattern;
+        }
+        cout<<endl;
+     }   */
+
+
+
+   /* for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++) {
+            cout<<"1 ";
+        }
+        cout<<endl;
+    }   */
+
+
+
+   /* for(int i=1;i<=n;i++) {
+        for (int j=1;j<=i;j++) {
+            cout<<"A ";
+        }
+        cout<<endl;
+    }        */
+
+
+
+   /* for(int i=1;i<=n;i++) {
+        for(int j=1;j<=i;j++) {
+            cout<<j;
+        }
+        cout<<endl;
+    }    */
+
+
+
+   /* for(int i= 1; i<=n;i++) {
+        char ch='A';
+         for(int j=1;j<=i;j++) {
+            cout<<ch;
+            ch++;
+
+         }
+         cout<<endl;
+    }   */
+
+
+
+     /* int num=1;
+    for (int i=1;i<=n;i++) {
+        for(int j=1;j<=i;j++) {
+            cout<<i;
+        }
+        cout<<endl;
+        
+    }     */
+
+
+
+   /* char ch='A';
+    for(int i=1;i<=n;i++){
+        for( int j=1;j<=i;j++) {
+            cout<<ch;
+        }
+        cout<<endl;
+        ch++;
+    }   */
+
+
+
+   /* int num=1;
+    for(int i=1;i<=n;i++) {
+        for(int j=1;j<=i;j++) {
+            cout<<num;
+            num++;
+        }
+        cout<<endl;
+     }          */
+
+
+
+    /* char ch='A';
+     for(int i=1;i<=n;i++){
+         for(int j=1;j<=i;j++) {
+            cout<<ch;
+            ch++;
+         }
+         cout<<endl;
+        }       */
+
+
+
+
+           //   3.            ********************Reverse Patterns*******************
+
+         
+
+
+        /* for (int i=1; i<=n;i++){
+            for (int j=i;j>=1;j--) {
+                cout<<j<<" ";
+            }
+            cout<<endl;
+        }     */
+
+
+
+
+        //     4.             *************************Inverted Triangle Pattern******************
+
+
+
+      /*  for(int i=0; i<n;i++){
+            for(int j=0; j<i;j++) {
+                cout<<" ";
+            } 
+            for(int j=0;j<n-i;j++) {
+                cout<<i+1;
+            }
+            cout<<endl;
+        }   */
